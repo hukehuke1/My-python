8f6731e995422f69b213cd220a80a0bde73a7656
hukehuke1 <hukehuke@sina.com>
2017-10-18 17:03:36 +0800

    Revert "update"
    This reverts commit 00d2aa7bd0b33af6d3b877fa6f10b7241829560b.

--- algorithm/pycache_/hex.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-if __name__ == "__main__":
-    n1 = 255
-    n2 = 1000
-    print('十六进制%s'%(hex(n1)))
-    print('十六进制%s'%(hex(n2)))
--- appium/Auty-master/README.md
+++
@@ -1,2 +0,0 @@
-# Auty
-关于Auty的信息请见这里：http://www.cnblogs.com/LanTianYou/p/5952472.html
